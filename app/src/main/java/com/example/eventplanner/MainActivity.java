8fd9d4b090a836f06a98229bde66b22408f1731c
Tea <belicc.tt@gmail.com>
2024-11-15 16:59:58 +0100

    Adding side bar to the home screen

--- app/src/main/java/com/example/eventplanner/MainActivity.java
+++ app/src/main/java/com/example/eventplanner/MainActivity.java
@@ -1,101 +1,37 @@
 package com.example.eventplanner;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.util.Log;
-
+import android.view.View;
+import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
-import com.example.eventplanner.R;
-
-import com.example.eventplanner.databinding.ActivityMainBinding;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
-    private BottomNavigationView bottomNavigationView;
-    private Fragment selectedFragment = new HomeFragment(); // Default fragment
+
+    private NavigationView navigationView;
+    private ImageView navigationIcon;
+    private boolean isNavigationViewVisible = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        bottomNavigationView = findViewById(R.id.bottom_navigation_view);
+        navigationView = findViewById(R.id.navigation_view);
+        navigationIcon = findViewById(R.id.navigation_icon);
 
-        // Set up the navigation listener
-        bottomNavigationView.setOnItemSelectedListener(item -> {
-            if (item.getItemId() == R.id.home) {
-                selectedFragment = new HomeFragment(); // Show Home Fragment
-            } else if (item.getItemId() == R.id.profile) {
-                selectedFragment = new ProfileFragment(); // Show Profile Fragment
-            } else if (item.getItemId() == R.id.settings) {
-                selectedFragment = new SettingsFragment(); // Show Settings Fragment
-            } else {
-                return false; // Return false if none of the items are matched
+        navigationIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isNavigationViewVisible) {
+                    // Hide the NavigationView
+                    navigationView.setVisibility(View.GONE);
+                } else {
+                    // Show the NavigationView
+                    navigationView.setVisibility(View.VISIBLE);
+                }
+                isNavigationViewVisible = !isNavigationViewVisible;
             }
-
-            // Perform fragment transaction
-            getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.frame_layout, selectedFragment)
-                    .commit();
-            return true;
         });
-
-        // Load the default fragment when the app starts
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.frame_layout, selectedFragment)
-                    .commit();
-        }
     }
 }
-/*
-public class MainActivity extends AppCompatActivity {
-
-    ActivityMainBinding binding;
-
-    @SuppressLint("NonConstantResourceId")
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(R.layout.activity_main);
-
-        // Set initial fragment
-        replaceFragment(new HomeFragment());
-
-        Log.d("AAAAAAAa", "binding.bottomNavigationView: " + binding.bottomNavigationView);
-
-        // Bottom Navigation Item selection listener
-        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
-
-            int itemId = item.getItemId();
-
-            Log.d("omg", "omg");
-
-            if (itemId == R.id.home) {
-                replaceFragment(new HomeFragment());
-            } else if (itemId == R.id.profile) {
-                replaceFragment(new ProfileFragment());
-            } else if (itemId == R.id.settings) {
-                replaceFragment(new SettingsFragment());
-            }
-            return true;  // If you handle the item, return true
-        });
-    }
-
-    // Helper method to replace fragments
-
-    private void replaceFragment(Fragment fragment) {
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.replace(R.id.frame_layout, fragment); // Replace with the container ID
-        transaction.addToBackStack(null); // Optional: Add to back stack if you want to allow back navigation
-        transaction.commit();
-    }
-*/
-
-
-
