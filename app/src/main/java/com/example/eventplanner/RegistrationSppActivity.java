6644fc0ba0b2c7c1047fd671f4161fb610563514
Tea <belicc.tt@gmail.com>
2024-11-17 14:35:16 +0100

    Commiting Registration Screens

--- app/src/main/java/com/example/eventplanner/RegistrationSppActivity.java
+++ app/src/main/java/com/example/eventplanner/RegistrationSppActivity.java
@@ -15,7 +15,7 @@ public class RegistrationSppActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.registration_spp_activity);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.registration_spp), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
