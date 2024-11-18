014b45c9f3922d2538d261f53b5187aa321dfa33
Tea <belicc.tt@gmail.com>
2024-11-18 03:28:19 +0100

    Commiting to develop branch

--- app/src/main/java/com/example/eventplanner/MainActivity.java
+++ app/src/main/java/com/example/eventplanner/MainActivity.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
@@ -20,6 +23,30 @@ public class MainActivity extends AppCompatActivity {
         navigationView = findViewById(R.id.navigation_view);
         navigationIcon = findViewById(R.id.navigation_icon);
 
+        // Set up the bottom navigation view listener
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation_view);
+        bottomNav.setOnNavigationItemSelectedListener(item -> {
+            Fragment selectedFragment = null;
+
+            // Determine which fragment to show based on the selected item
+            if (item.getItemId() == R.id.home) {
+                selectedFragment = new HomeFragment();
+            } else if (item.getItemId() == R.id.profile) {
+                selectedFragment = new ProfileFragment();
+            } else if (item.getItemId() == R.id.settings) {
+                selectedFragment = new SettingsFragment();
+            }
+
+            // Replace the current fragment with the selected fragment
+            if (selectedFragment != null) {
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.home_page_fragment, selectedFragment);
+                transaction.commit();
+            }
+
+            return true; // Return true to indicate the item selection was handled
+        });
+
         navigationIcon.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -33,5 +60,12 @@ public class MainActivity extends AppCompatActivity {
                 isNavigationViewVisible = !isNavigationViewVisible;
             }
         });
+
+        // Initialize with the HomeFragment by default if savedInstanceState is null
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.home_page_fragment, new HomeFragment())
+                    .commit();
+        }
     }
 }
