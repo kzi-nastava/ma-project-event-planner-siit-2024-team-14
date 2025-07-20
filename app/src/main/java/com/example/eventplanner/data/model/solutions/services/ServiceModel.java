9840f16371a1e401571daa70467ae99c8992c7fd
milicabosancic1 <milicabosancic1612@gmail.com>
2025-07-20 13:37:36 +0200

    reorganized folders and files

--- app/src/main/java/com/example/eventplanner/data/model/solutions/services/ServiceModel.java
+++ app/src/main/java/com/example/eventplanner/data/model/solutions/services/ServiceModel.java
@@ -8,11 +8,8 @@ import com.example.eventplanner.data.model.users.ProviderModel;
 import java.util.List;
 
 public class ServiceModel extends OfferingModel {
-    private int id;
-    private Category category;
-    private List<EventTypeModel> applicableEventTypes;
-    private double price;
-    private double discount;
+
+
     private List<String> imageURLs;
 
     private String visibility;
@@ -30,49 +27,6 @@ public class ServiceModel extends OfferingModel {
     private int maxDurationInMinutes;
     private int reservationPeriodInDays;
     private int cancellationPeriodInDays;
-
-    private ProviderModel provider;
-    private int providerId;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public Category getCategory() {
-        return category;
-    }
-
-    public void setCategory(Category category) {
-        this.category = category;
-    }
-
-    public List<EventTypeModel> getApplicableEventTypes() {
-        return applicableEventTypes;
-    }
-
-    public void setApplicableEventTypes(List<EventTypeModel> applicableEventTypes) {
-        this.applicableEventTypes = applicableEventTypes;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
-
-    public double getDiscount() {
-        return discount;
-    }
-
-    public void setDiscount(double discount) {
-        this.discount = discount;
-    }
 
     public List<String> getImageURLs() {
         return imageURLs;
@@ -186,19 +140,4 @@ public class ServiceModel extends OfferingModel {
         this.cancellationPeriodInDays = cancellationPeriodInDays;
     }
 
-    public ProviderModel getProvider() {
-        return provider;
-    }
-
-    public void setProvider(ProviderModel provider) {
-        this.provider = provider;
-    }
-
-    public int getProviderId() {
-        return providerId;
-    }
-
-    public void setProviderId(int providerId) {
-        this.providerId = providerId;
-    }
 }
